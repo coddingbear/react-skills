e114fba63b36dda3dafd634ad34b19d7a86f8817
CodingBear <surfwon@naver.com>
2018-10-18 22:29:09 +0900

    2.4.3 if 문 대신 조건부 연산자

--- chapter02/hello-react/src/App.js
+++ chapter02/hello-react/src/App.js
@@ -2,10 +2,12 @@ import React, { Component, Fragment } from "react";
 class App extends Component {
   render() {
     const text = "당신은 어썸한가요?";
+    const condition = true;
     return (
       <div>
         <h1>리액트 안녕</h1>
         <h2>{text}</h2>
+        {condition ? "참" : "거짓"}
       </div>
     );
   }
