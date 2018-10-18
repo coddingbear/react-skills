60b55e2a06e95cb06dcee11b23109f8841701e3e
CodingBear <surfwon@naver.com>
2018-10-18 22:31:15 +0900

    2.4.4 &&를 사용한 조건부 렌더링

--- chapter02/hello-react/src/App.js
+++ chapter02/hello-react/src/App.js
@@ -2,12 +2,13 @@ import React, { Component, Fragment } from "react";
 class App extends Component {
   render() {
     const text = "당신은 어썸한가요?";
-    const condition = true;
+    const condition = false;
     return (
       <div>
         <h1>리액트 안녕</h1>
         <h2>{text}</h2>
-        {condition ? "참" : "거짓"}
+        {condition ? "보여주세요" : null}
+        {condition && "보여주세요"}
       </div>
     );
   }
