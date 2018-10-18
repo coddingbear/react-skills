4e6d413d088d258ae71d2cf9b73b5facb648b765
CodingBear <surfwon@naver.com>
2018-10-18 22:25:50 +0900

    2.4.1 Fragment 사용

--- chapter02/hello-react/src/App.js
+++ chapter02/hello-react/src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
+        {/* react v16 이상에서 div 감싸지 않고 여러 요소를 렌더링할 때 Fragment 컴포넌트를 사용한다. */}
         <h1>리액트 안녕</h1>
         <h2>당신은 어썸한가요?</h2>
-      </div>
+      </Fragment>
     );
   }
 }
