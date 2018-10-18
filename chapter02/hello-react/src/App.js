4b7a22bf4af2633948c6fd9b4a7f42c21ecd2caa
CodingBear <surfwon@naver.com>
2018-10-18 22:22:01 +0900

    2.4.1 감싸인 요소

--- chapter02/hello-react/src/App.js
+++ chapter02/hello-react/src/App.js
@@ -1,28 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <footer className="App-footer">
-          <p>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </footer>
+      <div>
+        <h1>리액트 안녕</h1>
+        <h2>당신은 어썸한가요?</h2>
       </div>
     );
   }
