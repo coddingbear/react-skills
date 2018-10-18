ec24bb50ecc9c348ffac94df7f2f62bde3058d8c
CodingBear <surfwon@naver.com>
2018-10-18 22:27:48 +0900

    2.4.2  자바스크립트 표현

--- chapter02/hello-react/src/App.js
+++ chapter02/hello-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from "react";
 class App extends Component {
   render() {
+    const text = "당신은 어썸한가요?";
     return (
-      <Fragment>
-        {/* react v16 이상에서 div 감싸지 않고 여러 요소를 렌더링할 때 Fragment 컴포넌트를 사용한다. */}
+      <div>
         <h1>리액트 안녕</h1>
-        <h2>당신은 어썸한가요?</h2>
-      </Fragment>
+        <h2>{text}</h2>
+      </div>
     );
   }
 }
