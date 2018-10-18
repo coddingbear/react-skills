60b976121bbae15252150a0693e83b60c73572a1
CodingBear <surfwon@naver.com>
2018-10-18 23:06:07 +0900

    3.2.3 props 기본값 설정

--- chapter03/hello-react/src/MyComponent.js
+++ chapter03/hello-react/src/MyComponent.js
@@ -1,7 +1,12 @@
-// 3.2.1 JSX 내부에서 props 렌더링
+// 3.2.3 props 기본 값 설정: defaultProps
 import React, {Component} from 'react';
 
 class MyComponent extends Component {
+    /* props 기본값 설정 2
+    static defaultProps = {
+        name : '기본 이름'
+    }
+    */
     render() {
         return (
             <div>
@@ -9,6 +14,11 @@ class MyComponent extends Component {
             </div>
         )
     }
+}
+
+// props 기본값 설정 1
+MyComponent.defaultProps = {
+    name: '기본 이름'
 }
 
 export default MyComponent;
