8b223341e4019a04be03cc0bd86e400da4ebecb7
Nullgom <surfwon@gmail.com>
2018-10-19 12:16:59 +0900

    6.4 응용 - 삭제 기능 filter로 구현

--- chapter06/hello-react/src/IterationSample.js
+++ chapter06/hello-react/src/IterationSample.js
@@ -27,18 +27,9 @@ class IterationSample extends Component {
   handleRemove = (index) => {
     // 편의상 name의 레퍼런스를 미리 만듭니다.
     const {names} = this.state;
-
-    /* 배열을 자르는 내장 함수 slice와 
-      전개 연산자(...)를 사용하여 index번째 값을 제외한 값들을 배열에 넣어줍니다.
-    */
     this.setState({
-      names: [
-        ...names.slice(0, index),
-        ...names.slice(index + 1, names.length)
-      ]
-      /* ES5 문법 코드
-      this.state.names.slice(0,index).concat(this.state.names.slice(index+1, this.state.names.length))
-      */
+      // filter로 index번째를 제외한 원소만 있는 새 배열을 생성
+      names: names.filter((item, i) => i !== index)
     });
   }
 
