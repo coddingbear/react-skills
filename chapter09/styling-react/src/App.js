f040d3a8f61450d8275b99b9c99dbd1a5cc79f6e
Nullgom <surfwon@gmail.com>
2018-10-19 18:01:36 +0900

    9.2.4  Sass 라이브러리 - include-media, open-color

--- chapter09/styling-react/src/App.js
+++ chapter09/styling-react/src/App.js
@@ -1,26 +1,15 @@
 // 9.2 Sass 사용하기
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import styles from './App.scss'; // classNames 사용 할때는 App.css 파일로 불러오기
-
-const cx = classNames.bind(styles);
+import Button from './components/Button';
 
 class App extends Component {
   render() {
-    const isBlue = true;
     return (
-      // isBlue값이 true일 때만 blue 클래스 적용
-     <div className={cx('box', {blue: isBlue})}>
-        <div className={cx('box-inside')}/>
-     </div>
-    );
+      <div>
+        <Button>버튼</Button>
+      </div>
+    )
   }
 }
 
 export default App;
-
-/* 9.2.1 프로젝트에 Sass 적용
-1. node-sass, sass-loader 설치
-  $ yarn add node-sass sass-loader
-2. config/webpack.config.dev.js 수정  sass-loader 부분 수정
-*/
---
+++ chapter09/styling-react/src/components/Button/index.js
@@ -0,0 +1,6 @@
+import Button from './Button';
+export default Button;
+
+/* 위 코드를 한 줄로 줄이면
+export {default} from './Button';
+*/
